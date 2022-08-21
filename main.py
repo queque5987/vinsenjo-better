4e4a3aff6ef337d11ebcd7f2e8cecb860e8347ae
queque5987 <pyw5987@gmail.com>
2022-08-21 11:59:34 +0900

    20220821 1159 requirements change

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 
-import tensorflow as tf
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 import numpy as np
 
-import rtvc_main
-from encoder import inference as encoder
-from synthesizer.inference import Synthesizer
+# import rtvc_main
+# from encoder import inference as encoder
+# from synthesizer.inference import Synthesizer
 
-args = rtvc_main.rtvc_args()
+# args = rtvc_main.rtvc_args()
 # synthesizer = Synthesizer(args.syn_model_fpath)
-encoder.load_model(args.enc_model_fpath)
+# encoder.load_model(args.enc_model_fpath)
 
 app = FastAPI()
 
@@ -23,10 +21,10 @@ class UserInput(BaseModel):
 async def index():
     return {"Message": "This is Index"}
 
-@app.post('/predict/')
-async def predict(UserInput: UserInput):
+# @app.post('/predict/')
+# async def predict(UserInput: UserInput):
 
-    preprocessed_wav = encoder.preprocess_wav([UserInput.user_input])
-    embed = encoder.embed_utterance(preprocessed_wav)
+#     preprocessed_wav = encoder.preprocess_wav([UserInput.user_input])
+#     embed = encoder.embed_utterance(preprocessed_wav)
 
-    return {"embed": float(embed)}
+#     return {"embed": float(embed)}
